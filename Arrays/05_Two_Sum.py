ea99e76353dc5e53e5e936c36902b557540db328
Gaurav Chaudhary <gauravch410@gmail.com>
2026-07-06 21:47:40 +0530

    Solved 5 problems of array from Leetcode
    Solving problems from leetcode is fun. Helps to build logic and it introduce you to real coding.

---
+++ Arrays/05_Two_Sum.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+            
+# print(Solution().twoSum([2,3,4,5],7))
