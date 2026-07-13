7dcb588692efd37248aecd09a703a9a4aefd2ae6
Gaurav Chaudhary <gauravch410@gmail.com>
2026-07-13 13:47:17 +0530

    LeetCode Problem: #169 - Majority Element Finds the element that appears more than n/2 times in an array.
    Identifies the majority element in an array. This solution is optimized using Boyer-Moore Voting Algorithm, which tracks a candidate element and a counter, achieving a highly efficient $O(n)$ time complexity and $O(1)$ auxiliary space.

---
+++ Arrays/29_Majority_Element.py
@@ -0,0 +1,24 @@
+"""
+Given an array nums of size n, return the majority element.
+
+The majority element is the element that appears more than ⌊n / 2⌋ times. 
+You may assume that the majority element always exists in the array.
+"""
+
+class Solution:
+    def majorityElement(self, nums):
+        candidate = None
+        count = 0
+
+        for num in nums:
+            if count == 0:
+                candidate = num
+
+            if num == candidate:
+                count += 1
+            else:
+                count -= 1
+
+        return candidate
+
+print(Solution().majorityElement([2,2,3,4,5,5,6]))
