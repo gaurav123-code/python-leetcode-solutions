0b384ef52be9bc1a732f816f761b26118f0f1e34
Gaurav Chaudhary <gauravch410@gmail.com>
2026-07-16 18:19:40 +0530

    Create 30_GCD_of_Odd_Even_Sum.py

---
+++ Arrays/30_GCD_of_Odd_Even_Sum.py
@@ -0,0 +1,16 @@
+"""
+You are given an integer n. Your task is to compute the GCD (greatest common divisor) 
+of two values:
+
+sumOdd: the sum of the smallest n positive odd numbers.
+
+sumEven: the sum of the smallest n positive even numbers.
+
+Return the GCD of sumOdd and sumEven.
+"""
+class Solution(object):
+    def gcdOfOddEvenSums(self, n):
+        return n
+
+print(Solution().gcdOfOddEvenSums(4))
+print(Solution().gcdOfOddEvenSums(5))
