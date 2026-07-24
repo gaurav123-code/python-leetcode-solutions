8da7caed5d92d45e3b7f5b009e898034f631ac05
Gaurav Chaudhary <gauravch410@gmail.com>
2026-07-24 11:38:08 +0530

    Create 01_Valid_Sudoku.py

---
+++ HashMap/01_Valid_Sudoku.py
@@ -0,0 +1,54 @@
+"""
+Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according 
+to the following rules:
+
+Each row must contain the digits 1-9 without repetition.
+Each column must contain the digits 1-9 without repetition.
+Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+Note:
+
+A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+Only the filled cells need to be validated according to the mentioned rules.
+"""
+
+class Solution(object):
+    def isValidSudoku(self, board):
+
+        rows = [set() for _ in range(9)]
+        cols = [set() for _ in range(9)]
+        boxes = [set() for _ in range(9)]
+
+        for row in range(9):
+            for col in range(9):
+
+                num = board[row][col]
+
+                if num == ".":
+                    continue
+
+                box = (row // 3) * 3 + (col // 3)
+
+                if num in rows[row]:
+                    return False
+
+                if num in cols[col]:
+                    return False
+
+                if num in boxes[box]:
+                    return False
+
+                rows[row].add(num)
+                cols[col].add(num)
+                boxes[box].add(num)
+
+        return True
+
+print(Solution().isValidSudoku([["5","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]))
