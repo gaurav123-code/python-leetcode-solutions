7fdb8768e81a0c1dc068e5eac360b3911d4929ff
Gaurav Chaudhary <gauravch410@gmail.com>
2026-07-27 23:43:55 +0530

    Create 14_Intersection_of_Two_Linked_List.py

---
+++ HashMap/14_Intersection_of_Two_Linked_List.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def getIntersectionNode(self, headA, headB):
+        pA = headA
+        pB = headB
+
+        while pA != pB:
+            if pA:
+                pA = pA.next
+            else:
+                pA = headB
+
+            if pB:
+                pB = pB.next
+            else:
+                pB = headA
+
+        return pA
+    
