febd4d48c7eac51e47dcdec75094589d6df8fafa
Gaurav Chaudhary <gauravch410@gmail.com>
2026-07-30 23:29:17 +0530

    Create 25_Ugly_Number_II.py

---
+++ HashMap/25_Ugly_Number_II.py
@@ -0,0 +1,31 @@
+"""
+An ugly number is a positive integer whose prime factors are limited to 2, 3, and 5.
+
+Given an integer n, return the nth ugly number.
+"""
+
+class Solution(object):
+    def nthUglyNumber(self, n):
+        ugly = [1] * n
+
+        i2 = i3 = i5 = 0
+
+        for i in range(1, n):
+            next2 = ugly[i2] * 2
+            next3 = ugly[i3] * 3
+            next5 = ugly[i5] * 5
+
+            ugly[i] = min(next2, next3, next5)
+
+            if ugly[i] == next2:
+                i2 += 1
+
+            if ugly[i] == next3:
+                i3 += 1
+
+            if ugly[i] == next5:
+                i5 += 1
+
+        return ugly[-1]
+    
+print(Solution().nthUglyNumber(10))
