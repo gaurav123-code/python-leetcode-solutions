b7f293baf8f899f8811b09d55417ab7639507a3f
Gaurav Chaudhary <gauravch410@gmail.com>
2026-08-04 20:08:41 +0530

    Create 01_Implement_Stack_Using_Queue.py

---
+++ Queue/01_Implement_Stack_Using_Queue.py
@@ -0,0 +1,56 @@
+"""
+Implement a last-in-first-out (LIFO) stack using only two queues. The implemented stack should
+ support all the functions of a normal stack (push, top, pop, and empty).
+
+Implement the MyStack class:
+
+void push(int x) Pushes element x to the top of the stack.
+int pop() Removes the element on the top of the stack and returns it.
+int top() Returns the element on the top of the stack.
+boolean empty() Returns true if the stack is empty, false otherwise.
+Notes:
+
+You must use only standard operations of a queue, which means that only push to back, peek/pop 
+from front, size and is empty operations are valid.
+Depending on your language, the queue may not be supported natively. You may simulate a queue 
+using a list or deque (double-ended queue) as long as you use only a queue's standard operations.
+ 
+
+"""
+
+from collections import deque
+
+class MyStack(object):
+
+    def __init__(self):
+        self.q1 = deque()
+        self.q2 = deque()
+
+    def push(self, x):
+        
+        self.q2.append(x)
+
+        while self.q1:
+            self.q2.append(self.q1.popleft())
+
+        self.q1, self.q2 = self.q2, self.q1
+
+    def pop(self):
+        
+        return self.q1.popleft()
+
+    def top(self):
+        return self.q1[0]
+
+    def empty(self):
+        
+        return len(self.q1) == 0
+    
+    
+obj = MyStack()
+obj.push(1)
+obj.push(2)
+
+print(obj.top())    # 2
+print(obj.pop())    # 2
+print(obj.empty())  # False
